158ab1f7d6d8ad55e2a1bfbd676f08f55f764558
kzeltner29 <113716672+kzeltner29@users.noreply.github.com>
2024-09-23 16:42:00 -0400

    Update Oct7th.py

--- Oct7th.py
+++ Oct7th.py
@@ -59,7 +59,7 @@ if category != 'Select a Category':
     
         # Show a line chart for the selected subcategories
         st.write(f"Sales of {', '.join(subcategory)} over time")
-        st.line_chart(selected_sales)
+        st.line_chart(selected_sales, y="Sales", x='Date')
 
 
 # Calculate the overall profit margin across all products
